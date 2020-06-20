f698e5ad0aed316743cf471e74d536300f179459
ext_s.singh <santoshk.singh@indusnet.co.in>
2020-06-20 14:26:45 +0530

    Initial commit a demo project

---
+++ DemoProject/DemoProject/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Santosh on 20/06/20.
+//  Copyright © 2020 Santosh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        print("This is a print statement.")
+    }
+
+
+}
+
